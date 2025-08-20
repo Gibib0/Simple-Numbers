dfaa10109bd26b64ee0d20420d6f48b28fe126d1
Anastasia <anast111antonova@gmail.com>
2025-08-20 22:33:01 +0300

    Add check loop to check is number simple; add label

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 'use strict'
 
-for(let num = 2; num <= 100; num++) {
-	
+mainLoop:for(let num = 2; num <= 100; num++) {
+	checkLoop:for(let i = 2; i < num; i++) {
+		if(num % i === 0) {
+			continue mainLoop
+		}
+	}
 }
